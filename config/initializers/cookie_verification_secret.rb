27415cf5b0bc5c030efd54da65b5084f65a16f80
Richard Lee <rblee88@gmail.com>
2011-12-17 15:11:42 -0600

    Deleted everything because it's using the wrong version of rails

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '78eb0f9dcbdc78058d2d1f7e5e5a762cc4c35ec2aa91b7aaaf53f4ce16da934740222d4dd41257334a2bb8d36118f1750dd2f88ed80083da2f2620ccca466535';
