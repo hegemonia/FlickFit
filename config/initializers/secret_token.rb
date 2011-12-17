17a6b097700937cbb86154396a2ebc255d784254
Richard Lee <rblee88@gmail.com>
2011-12-17 15:13:41 -0600

    New Rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlickFit::Application.config.secret_token = '9187db61d9d7d7bdddbe13ff697974dfb64a02796bc5e7f5baf892e2685330b1551be0e9e9f9983551b8206b03434734f6cc5b7ed7218bf50e9042f49af5916a'
