252e623b4fd3fba77b876bb40245de808487996d
Richard Lee <rblee88@gmail.com>
2012-03-24 16:17:11 -0500

    Add reviewing functionality

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+	before_filter :authenticate_user!
+
+	def index
+		@user = current_user
+		existing_reviews = Review.where :user_id => @user.id
+		movies = existing_reviews.any? ? Movie.where("id not in (?)", existing_reviews.collect {|review| review.movie_id}) : Movie.all
+																		
+
+		new_reviews = []
+		movies.each do |movie|
+			new_reviews << Review.new(:movie => movie, :user => @user)
+		end
+
+		@reviews = existing_reviews + new_reviews
+	end
+
+	def create
+		Review.create(params[:review].merge({:user_id => current_user.id}))
+		redirect_to reviews_path
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ FlickFit::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :movies
+  resources :reviews
   # Sample resource route with options:
   #   resources :products do
   #     member do
