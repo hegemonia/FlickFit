7a57359e8c66ff59fd8e93ab626dc01c7f5c6884
Richard Lee <rblee88@gmail.com>
2012-03-24 13:26:08 -0500

    Config for PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120212064125) do
+ActiveRecord::Schema.define(:version => 20120324182215) do
+
+  create_table "movies", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
