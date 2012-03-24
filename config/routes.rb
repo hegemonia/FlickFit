b0012df016ad3949d8e5fcc4c89e52df5d8a32e8
Richard Lee <rblee88@gmail.com>
2012-03-24 16:59:36 -0500

    Change the root route to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ FlickFit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'movies#index'
+  root :to => 'reviews#index'
 
   # See how all your routes lay out with "rake routes"
 
