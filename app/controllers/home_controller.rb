d496f118418cefd44fd9ada6833294220862a3ae
Richard Lee <rblee88@gmail.com>
2012-04-14 18:19:49 -0500

    Minor refactoring of home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,28 +3,35 @@ class HomeController < ApplicationController
 	def index
 		@user = current_user
 		existing_reviews = Review.where :user_id => @user.id
-		movies = existing_reviews.any? ? Movie.where("id not in (?)", existing_reviews.collect {|review| review.movie_id}) : Movie.all
-																		
+
+		unreviewed_movies = existing_reviews.any? ? Movie.where("id not in (?)", existing_reviews.collect {|review| review.movie_id}) : Movie.all
 
 		new_reviews = []
-		movies.each do |movie|
+		unreviewed_movies.each do |movie|
 			new_reviews << Review.new(:movie => movie, :user => @user)
 		end
 
 		@reviews = (existing_reviews + new_reviews).sort_by {|review| review.movie.title}
+		@peer_scores = calculate_peer_scores @reviews
+	end
 
+	private
+
+	def calculate_peer_scores reviews
 		comparator = ReviewComparator.new
 		all_users = User.all
 		user_comparison_hash = {}
 
-		reviewed_movie_ids = existing_reviews.map(&:movie_id)
+		reviewed_movie_ids = reviews.map(&:movie_id)
+
 		all_users.each do |user|
 			other_user_reviews = user.reviews.select {|review| reviewed_movie_ids.include? review.movie_id }
 			other_reviewed_movie_ids = other_user_reviews.map(&:movie_id)
 
-			current_user_reviews = existing_reviews.select {|review| other_reviewed_movie_ids.include? review.movie_id}
-			user_comparison_hash["#{user.email}"] = comparator.calculate_similarity(current_user_reviews, other_user_reviews) if !other_user_reviews.empty?
+			reviews_of_the_same_movies = reviews.select {|review| other_reviewed_movie_ids.include? review.movie_id}
+
+			user_comparison_hash["#{user.email}"] = comparator.calculate_similarity(reviews_of_the_same_movies, other_user_reviews) if !other_user_reviews.empty?
 		end
-		@peer_scores = user_comparison_hash.sort_by {|k,v| v }.reverse
+		user_comparison_hash.sort_by {|k,v| v }.reverse
 	end
 end
