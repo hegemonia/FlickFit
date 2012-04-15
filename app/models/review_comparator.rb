2e5a28a26a19d51ebdc66faf553a9d8a9c922bc5
Richard Lee <rblee88@gmail.com>
2012-04-15 10:50:18 -0500

    Fix whitespace issues

--- app/models/review_comparator.rb
+++ app/models/review_comparator.rb
@@ -1,30 +1,30 @@
 class ReviewComparator
 	def calculate_similarity reviews1_arg, reviews2_arg
 		raise(ReviewCountMismatch, "The length of each review collection must be equal") if reviews1_arg.length != reviews2_arg.length
-    reviews1 = reviews1_arg.dup
-    reviews2 = reviews2_arg.dup
+		reviews1 = reviews1_arg.dup
+		reviews2 = reviews2_arg.dup
 
-    reviews1.sort_by! {|review| review.movie_id }
-    reviews2.sort_by! {|review| review.movie_id }
-    movie_count = reviews1.length
-    movie_scores = []
-    average_confidence_scores = []
-    while reviews1.any?
-	    movie_score, confidence = calculate_movie_similarity(reviews1.shift, reviews2.shift)
-	    average_confidence_scores << confidence
-	    movie_scores << movie_score
-	  end
-    weighted_score = movie_scores.sum / average_confidence_scores.sum
+		reviews1.sort_by! {|review| review.movie_id }
+		reviews2.sort_by! {|review| review.movie_id }
+		movie_count = reviews1.length
+		movie_scores = []
+		average_confidence_scores = []
+		while reviews1.any?
+			movie_score, confidence = calculate_movie_similarity(reviews1.shift, reviews2.shift)
+			average_confidence_scores << confidence
+			movie_scores << movie_score
+		end
+		weighted_score = movie_scores.sum / average_confidence_scores.sum
 
-    10 - (weighted_score + (4.0 / movie_count**2))
- 	end
+		10 - (weighted_score + (4.0 / movie_count**2))
+	end
 
- 	def calculate_movie_similarity movie1, movie2
- 		square_of_rating_difference = (movie1.rating.to_f - movie2.rating.to_f)**2
- 		average_confidence = (movie1.confidence.to_f + movie2.confidence.to_f)/2.0
+	def calculate_movie_similarity movie1, movie2
+		square_of_rating_difference = (movie1.rating.to_f - movie2.rating.to_f)**2
+		average_confidence = (movie1.confidence.to_f + movie2.confidence.to_f)/2.0
 
- 		return (square_of_rating_difference * average_confidence), average_confidence
- 	end
+		return (square_of_rating_difference * average_confidence), average_confidence
+	end
 end
 
 class ReviewCountMismatch; end
