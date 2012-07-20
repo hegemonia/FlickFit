671a2956e89566c0124cf79b3ab431818de1bc44
Luiz Ribeiro <luizfar@gmail.com>
2012-07-20 11:26:25 -0300

    Increasing synopsis field maximum length to 300 characters.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
   has_and_belongs_to_many :genres
 
   validates_uniqueness_of :title
-  validates :synopsis, :length => { :maximum => 140 }
+  validates :synopsis, :length => { :maximum => 300 }
   validates :year, :numericality => { :only_integer => true, :greater_than => 999, :less_than => 10000 }
   validates :runtime, :numericality => { :only_integer => true, :greater_than => 99, :less_than => 1000 }
 end
