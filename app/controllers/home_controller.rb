c562db54065b0a71bce4e648b6f0ba2767feb001
Richard Lee <rlee@thoughtworks.com>
2012-12-26 12:15:20 -0600

    Organize css and style filter bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,9 +8,9 @@ class HomeController < ApplicationController
   DEFAULT_PAGE_NUMBER = 1
 
   def index
-    genre = Genre::Filters.include?(params[:genre]) ? params[:genre] : nil
-    existing_reviews = (genre.present? ? Review.with_movie_genre(genre).uniq : Review).where(:user_id => current_user.id)
-    movie_relation = genre.present? ? Movie.with_genre(genre) : Movie
+    @genre = Genre::Filters.include?(params[:genre]) ? params[:genre] : nil
+    existing_reviews = (@genre.present? ? Review.with_movie_genre(@genre).uniq : Review).where(:user_id => current_user.id)
+    movie_relation = @genre.present? ? Movie.with_genre(@genre) : Movie
 
     unreviewed_movies = existing_reviews.any? ? movie_relation.where("movies.id not in (?)", existing_reviews.collect {|review| review.movie_id}) : movie_relation.all
 
@@ -23,6 +23,8 @@ class HomeController < ApplicationController
     current_page = params[:page].present? ? params[:page].to_i : DEFAULT_PAGE_NUMBER
 
     all_existing_reviews_for_user = Review.where(:user_id => current_user.id)
+
+    @genres = Genre::Filters - ["Action", "Adventure"]
     @peer_scores = calculate_peer_scores all_existing_reviews_for_user
 
     @reviews = all_reviews.paginate(:page => current_page, :per_page => REVIEWS_PER_PAGE)
